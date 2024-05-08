e68abf65fa2000babd33ebf1a7a8c0a2a23a6237
Xqhare <xqhare@web.de>
2024-05-09 01:49:26 +0200

    added read_json function

---
+++ src/json.rs
@@ -0,0 +1,16 @@
+use std::{path::Path, fs::File, io::{Error, Read, ErrorKind}};
+
+use json::{JsonValue, parse};
+
+pub fn read_json<P>(file_path: P) -> Result<JsonValue, Error> where P: AsRef<Path> {
+    let mut input = File::open(file_path)?;
+    let mut buffer: String = Default::default();
+    let _ = input.read_to_string(&mut buffer);
+    let out = parse(&buffer);
+    if out.is_ok() {
+        Ok(out.expect("Is Ok checked!"))
+    } else {
+        Err(Error::from(ErrorKind::InvalidData))
+    }
+}
+
