bae3cc3a91d14b6c3b914ed247226fc2b699dfc6
Xqhare <xqhare@web.de>
2024-05-10 01:41:42 +0200

    no real work

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,9 @@
+use ilmarinen::WordSmith;
+
 
 #[test]
 fn debug_test() {
     let t = ilmarinen::WordSmith::default();
     let tt = ilmarinen::WordSmith::test_main();
+    let ttt = WordSmith::
 }
