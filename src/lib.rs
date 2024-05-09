87d1295d2bb3ac3bfb32592a2ce850ab208809b3
Xqhare <xqhare@web.de>
2024-05-09 02:18:30 +0200

    added all stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 
-mod ilmarinen;
+pub mod ilmarinen;
 mod json;
 mod lexical_unit_pool;
 
+use crate::ilmarinen::Ilmarinen;
+use crate::json::read_json;
 
+impl Ilmarinen {
+    pub fn test_main() {
+        let a = read_json("lib/gen_lib.json");
+        println!("{:?}", a);
+    }
+}
