d028207fcb93d3e9d5238679025332efa22071a4
Xqhare <xqhare@web.de>
2024-05-10 01:41:09 +0200

    started work on new()

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,48 @@
 
 mod json;
 mod lexical_unit_pool;
+mod lexical_unit_lagoon;
 
-use ::json::JsonValue;
+use std::io::{Error, ErrorKind};
+use std::path::Path;
 
-use crate::json::read_json;
+use crate::json::{read_json, construct_word_smith};
 
 use crate::lexical_unit_pool::LexicalUnitPool;
 
 
 #[derive(Debug, Clone)]
 pub struct WordSmith {
-    library_adj: LexicalUnitPool,
-}
-
-impl Default for WordSmith {
-    fn default() -> Self {
-        let gen_lib_path = "lib/gen_lib.json";
-        WordSmith { library_adj: LexicalUnitPool::default() }
-    }
-}
-
-impl From<JsonValue> for WordSmith {
-    fn from(value: JsonValue) -> Self {
-        todo!()
-    }
+    lexical_unit_lagoon: LexicalUnitLagoon,
 }
 
 impl WordSmith {
+    /// Takes in a folder path. It extends for the needed files on its own, they need to be present
+    /// in the supplied path though.
+    pub fn new(data_path: &Path) -> Result<WordSmith, Error> {
+        // I only check the path here, real logic is external in `construct_self()`.
+        if data_path.is_dir() {
+            let tmp = data_path.try_exists();
+            if let Ok(answ) = data_path.try_exists() {
+                match answ {
+                    true => { 
+                        match construct_word_smith(data_path) {
+                            Ok(word_smith) => { Ok(word_smith) },
+                            Err(error) => { Err(Error::new(ErrorKind::Other, error)) }
+                        }
+                    }
+                    false => { Err(Error::from(ErrorKind::NotFound)) },
+                }
+            } else {
+                Err(Error::from(ErrorKind::PermissionDenied))
+            }
+        } else {
+            Err(Error::other(format!("Invalid data. '{:?}' needs to exist and be a directory.", data_path)))
+        } 
+    }
+
     pub fn test_main() {
-        let a = read_json("lib/gen_lib.json");
+        let a = read_json("data/gen_lib.json");
         println!("{:?}", a);
     }
 }
