dcdb63457fb206cbcf03aa0da80d27bbf29a7e5b
Xqhare <xqhare@web.de>
2024-05-09 02:06:56 +0200

    added default for debugging

--- src/ilmarinen.rs
+++ src/ilmarinen.rs
@@ -1,19 +1,28 @@
 use json::JsonValue;
 
+use crate::{lexical_unit_pool::LexicalUnitPool, json::read_json};
+
 
 #[derive(Debug, Clone)]
 pub struct Ilmarinen {
-    library_adj: Vec<String>,
+    library_adj: LexicalUnitPool,
 }
 
 impl Default for Ilmarinen {
     fn default() -> Self {
-        todo!()
+        Ilmarinen { library_adj: LexicalUnitPool::default() }
     }
 }
 
 impl From<JsonValue> for Ilmarinen {
     fn from(value: JsonValue) -> Self {
         todo!()
+    }
+}
+
+impl Ilmarinen {
+    pub fn test_main() {
+        let a = read_json("lib/gen_lib.json");
+        println!("{:?}", a);
     }
 }
