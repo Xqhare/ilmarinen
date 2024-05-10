5372be108e03a66c40cf5146229fb08e7bb0939b
Xqhare <xqhare@web.de>
2024-05-10 02:55:04 +0200

    functional stub

--- src/json.rs
+++ src/json.rs
@@ -2,6 +2,10 @@ use std::{path::Path, fs::File, io::{Error, Read, ErrorKind}};
 
 use json::{JsonValue, parse};
 
+use crate::WordSmith;
+
+//use crate::WordSmith;
+
 pub fn read_json<P>(file_path: P) -> Result<JsonValue, Error> where P: AsRef<Path> {
     let mut input = File::open(file_path)?;
     let mut buffer: String = Default::default();
@@ -16,6 +20,14 @@ pub fn read_json<P>(file_path: P) -> Result<JsonValue, Error> where P: AsRef<Pat
 
 pub fn construct_word_smith(data_path: &Path) -> Result<WordSmith, Error>{
     let gen_lib_path = data_path.join("gen_lib.json");
-    let data = 
+    if let Ok(data) = read_json(gen_lib_path) {
+        for thing in data.entries() {
+            for entry in thing.1.entries() {
+                println!("{:?}", entry);
+            }
+        }
+    } else {
+        println!("PANIK!");
+    }
 }
     
