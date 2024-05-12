b7e5fef52db73a6469a8b7b5234c702219a342e5
Xqhare <xqhare@web.de>
2024-05-12 19:56:51 +0200

    wordsmith::new test added, changed debug_test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,11 +1,18 @@
 use std::path::Path;
 
-use ilmarinen::WordSmith;
+use ilmarinen::{WordSmith, MintingType};
 
 
 #[test]
 fn debug_test() {
+    let tt = WordSmith::test_main(MintingType::People);
+    let tm = WordSmith::test_main(MintingType::ShipName);
+    println!("FINAL RESULT : {:?} | {:?}", tt, tm);
+}
+
+#[test]
+fn new_wordsmith_test() { 
     let tt = WordSmith::new(Path::new("data/"));
-    println!("FINAL RESULT : {:?}", tt);
+    //println!("FINAL RESULT : {:?}", tt);
     assert!(tt.is_ok())
 }
