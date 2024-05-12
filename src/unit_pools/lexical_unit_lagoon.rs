916f0d82b0dc5dafbf0bcaba96093544f11f01c1
Xqhare <xqhare@web.de>
2024-05-12 11:22:04 +0200

    moved json decoding into new()

--- src/unit_pools/lexical_unit_lagoon.rs
+++ src/unit_pools/lexical_unit_lagoon.rs
@@ -1,3 +1,7 @@
+use std::{io::{Error, ErrorKind}, path::Path};
+
+use crate::jisard::read_json;
+
 use super::lexical_unit_pool::LexicalUnitPool;
 
 #[derive(Debug, Clone, Default)]
@@ -81,4 +85,124 @@ pub struct LexicalUnitLagoon {
     pub story_government_leader_title_0_mon: LexicalUnitPool,
     pub story_government_leader_title_01: LexicalUnitPool,
     pub story_government_leader_title_1: LexicalUnitPool,
+}
+
+impl LexicalUnitLagoon {
+    pub fn new(gen_lib_path: &Path) -> Result<LexicalUnitLagoon, Error> {
+        let mut abc: LexicalUnitPool = Default::default();
+
+        let mut place_relatives: LexicalUnitPool = Default::default(); 
+        let mut place_noun: LexicalUnitPool = Default::default();
+        let mut place_top_eng_nouns: LexicalUnitPool = Default::default();
+        let mut place_object: LexicalUnitPool = Default::default();
+        let mut place_single: LexicalUnitPool = Default::default();
+
+        let mut general_comp0: LexicalUnitPool = Default::default();
+        let mut general_comp1: LexicalUnitPool = Default::default();
+
+        let mut people_first_name: LexicalUnitPool = Default::default();
+        let mut people_last_name: LexicalUnitPool = Default::default();
+        let mut people_nickname: LexicalUnitPool = Default::default();
+        let mut people_title: LexicalUnitPool = Default::default();
+        let mut people_skill: LexicalUnitPool = Default::default();
+        let mut people_skill_level: LexicalUnitPool = Default::default();
+        let mut people_trait_ck2: LexicalUnitPool = Default::default();
+        let mut people_trait_ck3: LexicalUnitPool = Default::default();
+
+        let mut artifact_type: LexicalUnitPool = Default::default();
+        let mut artifact_adjective: LexicalUnitPool = Default::default();
+        let mut artifact_material: LexicalUnitPool = Default::default();
+        let mut artifact_quality: LexicalUnitPool = Default::default();
+        let mut artifact_dedication: LexicalUnitPool = Default::default();
+
+        let mut scene_actors: LexicalUnitPool = Default::default();
+
+        let mut timeline_qualifiers: LexicalUnitPool = Default::default();
+        match read_json(gen_lib_path) {
+            Ok(data) => {
+                for entry in data.entries() {
+                    println!("{:?}", entry);
+                    println!("");
+                    match entry.0 {
+                        "abc" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "place_relatives" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "place_noun" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "place_top_eng_nouns" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "place_object" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "place_single" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "general_comp0" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "general_comp1" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "people_first_name" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "people_last_name" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "people_nickname" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "people_title" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "people_skill" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "people_skill_level" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "people_trait_ck2" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "people_trait_ck3" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "artifact_type" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "artifact_adjective" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "artifact_material" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "artifact_quality" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "artifact_dedication" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "scene_actors" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        "timeline_qualifiers" => {
+                            abc = LexicalUnitPool::from(entry);
+                        },
+                        _ => {
+                            Err(Error::other(format!("Undeclared json list. {} (in {:?}) is not implemented!", entry.0, gen_lib_path )))
+                        }?
+                    };
+                }
+                Ok(LexicalUnitLagoon::default())
+            },
+            Err(error) => {
+                Err(Error::new(ErrorKind::Other, error))
+            },
+        }
+    }
 }
