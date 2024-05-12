eb8ea03454613652577670aae6e77cc09ea2c8aa
Xqhare <xqhare@web.de>
2024-05-12 19:56:17 +0200

    added all MintingTypes

---
+++ src/minting_type.rs
@@ -0,0 +1,17 @@
+
+#[derive(Debug, Clone, Copy)]
+pub enum MintingType {
+    PlaceName,
+    People,
+    Artifact,
+    Operation,
+    ShipName,
+    ShipClass,
+    Currency,
+    MetalAndAlloy,
+    Empire,
+    Government,
+    Language,
+    Numbers,
+
+}
