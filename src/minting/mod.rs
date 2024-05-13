4a30de207abd96fce8d7d7857d92c9e19409ff06
Xqhare <xqhare@web.de>
2024-05-13 19:44:42 +0200

    added minter functions

--- src/minting/mod.rs
+++ src/minting/mod.rs
@@ -1,3 +1,24 @@
 
 pub mod minting_type;
-mod minting_result;
+pub mod minting_result;
+
+pub mod minters {
+    use std::{sync::{Arc, Mutex}, thread, time::Duration};
+
+    use crate::{unit_pools::UnitArchipelago, MintingResult};
+
+    pub fn mint_place(data: Arc<UnitArchipelago>, out: Arc<Mutex<MintingResult>>) {
+        loop {
+            if let Ok(mut store) = out.try_lock() {
+                store.result.push("test".to_string());
+                break;
+            };
+            thread::sleep(Duration::from_millis(5));
+        }
+            
+    }
+
+    pub fn mint_people(data: Arc<UnitArchipelago>, out: Arc<Mutex<MintingResult>>) {
+        todo!("implement this")
+    }
+}
