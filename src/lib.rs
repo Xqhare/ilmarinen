c080e8ceac5f82e6c14b041ab141da8be2398470
Xqhare <xqhare@web.de>
2024-05-13 19:45:01 +0200

    added async support

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,21 @@
 mod jisard;
 mod unit_pools;
 mod minting;
+mod thread_pool;
 
-use std::{io::{Error, ErrorKind}, path::Path};
+use std::{io::{Error, ErrorKind}, path::Path, sync::{Arc, Mutex}, thread, time::Duration};
 
 pub use crate::minting::minting_type::MintingType; 
+pub use crate::minting::minting_result::MintingResult;
+use minting::minters::*;
+use thread_pool::ThreadPool;
 use unit_pools::UnitArchipelago;
 
 
 #[derive(Debug, Clone)]
 pub struct WordSmith {
-    lexical_unit_lagoon: UnitArchipelago,
+    unit_archipelago: Arc<UnitArchipelago>,
+    result: Arc<Mutex<MintingResult>>,
 }
 
 impl WordSmith {
@@ -24,7 +29,7 @@ impl WordSmith {
                 match answ {
                     true => { 
                         match UnitArchipelago::new(data_path) {
-                            Ok(unit_archipelago) => { Ok( WordSmith { lexical_unit_lagoon: unit_archipelago } ) },
+                            Ok(unit_archipelago) => { Ok( WordSmith { unit_archipelago: unit_archipelago.into(), result: Arc::new(Mutex::new(MintingResult::default())) } ) },
                             Err(error) => { Err(Error::new(ErrorKind::Other, error)) }
                         }
                     }
@@ -37,10 +42,67 @@ impl WordSmith {
             Err(Error::other(format!("Invalid data. '{:?}' needs to exist and be a directory.", data_path)))
         } 
     }
-/* 
-    pub fn mint(minting_type: MintingType, mint_amount: usize) -> MintingResult {
-        unimplemented!();
-    } */
+
+    /// Will only mint if mint_amount is larger than 0.
+    pub fn mint(&self, minting_type: MintingType, mint_amount: usize) -> Result<(), Error> {
+        if mint_amount > 0 {
+                let thread_pool = {
+                    if mint_amount <= 10 {
+                        ThreadPool::build(2)
+                    } else if mint_amount <= 50 {
+                        ThreadPool::build(10)
+                    } else if mint_amount <= 100 {
+                        ThreadPool::build(20)
+                    } else {
+                        let size = {
+                            if mint_amount / 5 > 250 {
+                                250
+                            } else {
+                                mint_amount / 5
+                            }
+                        };
+                        ThreadPool::build(size)
+                    }
+                };
+                if let Ok(pool) = thread_pool {
+                    for _n in 0..mint_amount {
+                        let data = self.unit_archipelago.clone();
+                        let out = self.result.clone();
+                        match minting_type {
+                            MintingType::PlaceName => pool.execute(|| {mint_place(data, out) }),
+                            MintingType::People => pool.execute(|| {mint_people(data, out)}),
+                            MintingType::Artifact => {},
+                            MintingType::Operation => {},
+                            MintingType::ShipName => {},
+                            MintingType::ShipClass => {},
+                            MintingType::Currency => {},
+                            MintingType::MetalAndAlloy => {},
+                            MintingType::Empire => {},
+                            MintingType::Government => {},
+                            MintingType::Language => {},
+                            MintingType::Numbers => {},
+                        }
+                    }
+                    // Don't have a better idea of waiting for all workers to finish.
+                    // Could check the length of the created result, that idea seems worse.
+                    loop {
+                        // I really don't know how long this should be. Maybe 100,
+                        // maybe 10, maybe mint_amount?
+                        thread::sleep(Duration::from_millis(10));
+                        if let Ok(store) = self.result.try_lock() {
+                            if store.result.len() == mint_amount {
+                                return Ok(());
+                            }
+                        };
+                    }
+                } else {
+                    Err(Error::other("Fatal runtime error, unable to create thread pool."))
+                }
+        } else {
+            Err(Error::other("Amount is less than 1!"))
+        }
+    }
+
 
     pub fn test_main(minting_type: MintingType) {
         println!("{:?}", minting_type);
