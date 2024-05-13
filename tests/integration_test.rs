3e91637a93ca26bca338f37ca99181ea15e4e6e4
Xqhare <xqhare@web.de>
2024-05-13 19:42:57 +0200

    stopped testing explicitly what is tested implicitly

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,12 +8,12 @@ fn debug_test() {
     let tt = WordSmith::test_main(MintingType::People);
     let tm = WordSmith::test_main(MintingType::ShipName);
     let t = WordSmith::test_main(MintingType::Empire);
-    println!("FINAL RESULT : {:?} | {:?} | {:?}", tt, tm, t);
+    // println!("FINAL RESULT : {:?} | {:?} | {:?}", tt, tm, t);
 }
 
 #[test]
-fn new_wordsmith_test() { 
-    let tt = WordSmith::new(Path::new("data/"));
-    //println!("FINAL RESULT : {:?}", tt);
-    assert!(tt.is_ok())
+fn mint_place_name_test() {
+    let tmp = WordSmith::new(Path::new("data/")).unwrap();
+    let tmp1 = tmp.mint(MintingType::PlaceName, 100000);
 }
+
