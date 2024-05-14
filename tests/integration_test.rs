da7f9772aed2089e4a66cde5051a38ead935d6d9
Xqhare <xqhare@web.de>
2024-05-15 01:20:27 +0200

    changed up the tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,18 +2,13 @@ use std::path::Path;
 
 use ilmarinen::{WordSmith, MintingType};
 
-
-#[test]
-fn debug_test() {
-    let tt = WordSmith::test_main(MintingType::People);
-    let tm = WordSmith::test_main(MintingType::ShipName);
-    let t = WordSmith::test_main(MintingType::Empire);
-    // println!("FINAL RESULT : {:?} | {:?} | {:?}", tt, tm, t);
-}
-
 #[test]
 fn mint_place_name_test() {
     let mut tmp = WordSmith::new(Path::new("data/")).unwrap();
-    let tmp1 = tmp.mint(MintingType::PlaceName, 100000);
+    let tmp1 = tmp.mint(MintingType::People, 100000);
+    /* for entry in tmp1.unwrap().result {
+        println!("{}", entry)
+    } */
+    assert!(tmp1.is_ok())
 }
 
