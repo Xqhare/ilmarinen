cdfc4e260805e8d97298de570a8109fea65e6dad
Xqhare <xqhare@web.de>
2024-05-15 01:17:21 +0200

    added in people_press

--- src/minting/mod.rs
+++ src/minting/mod.rs
@@ -1,11 +1,14 @@
 
 pub mod minting_type;
 pub mod minting_result;
+mod presses;
 
 pub mod minters {
     use std::{sync::{Arc, Mutex}, thread, time::Duration};
 
     use crate::{unit_pools::UnitArchipelago, MintingResult};
+
+    use super::presses::people_press::people_press;
 
     pub fn mint_place(data: Arc<UnitArchipelago>, out: Arc<Mutex<MintingResult>>) {
         loop {
@@ -20,7 +23,8 @@ pub mod minters {
     pub fn mint_people(data: Arc<UnitArchipelago>, out: Arc<Mutex<MintingResult>>) {
         loop {
             if let Ok(mut store) = out.try_lock() {
-                store.result.push("test".to_string());
+                let result = people_press(data).expect("SOMETHING WENT TERRIBLY WRONG!");
+                store.result.push(result);
                 break;
             };
             thread::sleep(Duration::from_micros(100));
