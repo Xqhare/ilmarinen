7951724c53e7bb48cebb0e39657188f98a283acb
Xqhare <xqhare@web.de>
2024-05-15 19:01:24 +0200

    finished mint_place()

--- src/minting/mod.rs
+++ src/minting/mod.rs
@@ -8,12 +8,13 @@ pub mod minters {
 
     use crate::{unit_pools::UnitArchipelago, MintingResult};
 
-    use super::presses::people_press::people_press;
+    use super::presses::{people_press::people_press, place_press::place_press};
 
     pub fn mint_place(data: Arc<UnitArchipelago>, out: Arc<Mutex<MintingResult>>, sleep_duration: Arc<Duration>, sleep_offset: u64) {
         loop {
             if let Ok(mut store) = out.try_lock() {
-                store.result.push("test".to_string());
+                let result = place_press(data).expect("SOMETHING WENT TERRIBLY WRONG!");
+                store.result.push(result);
                 break;
             };
             //println!("MINT PLACE BLOCKED");
