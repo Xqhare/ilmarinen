5debf98257dba147900717a560bc6c60ee38a749
Xqhare <xqhare@web.de>
2024-05-15 20:31:07 +0200

    moved in common code

--- src/minting/presses/common.rs
+++ src/minting/presses/common.rs
@@ -51,3 +51,67 @@ pub fn full_name_die(data: Arc<UnitArchipelago>) -> Result<String, Error> {
     }
 }
 
+pub fn nickname_type(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    Ok(
+        format!("{}",
+            if prelude::random_from_range(0, 2)? == 0 {
+                data.lexical_unit_lagoon.people_nickname.unit_pool[prelude::random_index(data.lexical_unit_lagoon.people_nickname.unit_pool.len())?].clone()
+            } else {
+                String::default()
+            }
+        )
+    )
+}
+
+pub fn first_name_type(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    if prelude::random_from_range(0, 1)? == 0 {
+        comp_name_type(data)
+    } else {
+        Ok(
+            data.lexical_unit_lagoon.people_first_name.unit_pool[prelude::random_index(data.lexical_unit_lagoon.people_first_name.unit_pool.len())?].clone()
+        )
+    }
+}
+
+pub fn last_name_type(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    if prelude::random_from_range(0, 1)? == 0 {
+        comp_name_type(data)
+    } else {
+        Ok(
+            data.lexical_unit_lagoon.people_last_name.unit_pool[prelude::random_index(data.lexical_unit_lagoon.people_last_name.unit_pool.len())?].clone()
+        )
+    }
+}
+
+pub fn skill_die(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    Ok(
+        format!("{} {}",
+            data.lexical_unit_lagoon.people_skill_level.unit_pool[prelude::random_index(data.lexical_unit_lagoon.people_skill_level.unit_pool.len())?],
+            data.lexical_unit_lagoon.people_skill.unit_pool[prelude::random_index(data.lexical_unit_lagoon.people_skill.unit_pool.len())?]
+        )
+    )
+}
+
+pub fn material_type(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    Ok(
+        data.lexical_unit_lagoon.artifact_material.unit_pool[prelude::random_index(data.lexical_unit_lagoon.artifact_material.unit_pool.len())?].clone()
+    )
+}
+
+pub fn artifact_type_type(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    Ok(
+        data.lexical_unit_lagoon.artifact_type.unit_pool[prelude::random_index(data.lexical_unit_lagoon.artifact_type.unit_pool.len())?].clone()
+    )
+}
+
+pub fn adjective_type(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    Ok(
+        data.lexical_unit_lagoon.artifact_adjective.unit_pool[prelude::random_index(data.lexical_unit_lagoon.artifact_adjective.unit_pool.len())?].clone()
+    )
+}
+
+pub fn quality_type(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    Ok(
+        data.lexical_unit_lagoon.artifact_quality.unit_pool[prelude::random_index(data.lexical_unit_lagoon.artifact_quality.unit_pool.len())?].clone()
+    )
+}
