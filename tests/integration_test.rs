ca6e0242f0fd2d4a16f4277768e4b6084f3bbb4e
Xqhare <xqhare@web.de>
2024-05-15 19:22:20 +0200

    added language into tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,13 +3,15 @@ use std::path::Path;
 use ilmarinen::{WordSmith, MintingType};
 
 #[test]
-fn mint_place_name_test() {
+fn word_smith_main_test() {
     let mut word_smith = WordSmith::new(Path::new("data/")).unwrap();
     let people = word_smith.mint(MintingType::People, 100000);
     assert!(people.is_ok());
     let place = word_smith.mint(MintingType::PlaceName, 100000);
     assert!(place.is_ok());
-    for entry in place.unwrap().result {
+    let language = word_smith.mint(MintingType::Language, 100000);
+    assert!(language.is_ok());
+    for entry in language.unwrap().result {
         println!("{}", entry)
     };
 }
