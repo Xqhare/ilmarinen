c55dde39f74463ad0a78a0fd1a8667520d42f512
Xqhare <xqhare@web.de>
2024-05-15 19:04:21 +0200

    added place into tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,11 +4,13 @@ use ilmarinen::{WordSmith, MintingType};
 
 #[test]
 fn mint_place_name_test() {
-    let mut tmp = WordSmith::new(Path::new("data/")).unwrap();
-    let tmp1 = tmp.mint(MintingType::People, 100000);
-    /* for entry in tmp1.unwrap().result {
+    let mut word_smith = WordSmith::new(Path::new("data/")).unwrap();
+    let people = word_smith.mint(MintingType::People, 100000);
+    assert!(people.is_ok());
+    let place = word_smith.mint(MintingType::PlaceName, 100000);
+    assert!(place.is_ok());
+    for entry in place.unwrap().result {
         println!("{}", entry)
-    } */
-    assert!(tmp1.is_ok())
+    };
 }
 
