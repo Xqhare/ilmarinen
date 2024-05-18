0c33447eb099bf3051f23e37a4f2473f80e6f19b
Xqhare <xqhare@web.de>
2024-05-18 21:18:42 +0200

    created all empire generators

--- src/minting/presses/empire_press.rs
+++ src/minting/presses/empire_press.rs
@@ -1,29 +1,49 @@
 use std::{sync::Arc, io::Error};
 
+use tyche::prelude;
+
 use crate::unit_pools::UnitArchipelago;
 
+use self::empire::empire_name_die;
+
+use super::{common::{formal_name_die, language_name_die, full_place_name_die}, government_press::government_press};
+
 pub fn empire_press(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    let name = empire_name_die(data.clone())?;
+    let currency = "";
+    let area = prelude::random_from_range(1, 10000000)?;
+    let pop = prelude::random_from_range(1, 1000000)?;
     Ok(
-        format!("{}{}. {}",
-        "",
-        "",
-        ""
+        format!("Name: {}; Leader: {}; Capital: {}; Official language: {}; Population: {}; Area: {}; Currency: {}; Government: {}",
+        name,
+        formal_name_die(data.clone())?,
+        full_place_name_die(data.clone())?,
+        language_name_die(data.clone())?,
+        pop,
+        area,
+        currency,
+        government_press(data, &name)?
         )
     )
 }
 
 mod empire {
-    use std::{io::Error, sync::Arc, ops::Add};
+    use std::{io::Error, sync::Arc};
 
     use tyche::prelude;
 
-    use crate::{unit_pools::UnitArchipelago, minting::presses::common::simple_name_type};
+    use crate::{unit_pools::UnitArchipelago, minting::presses::common::{simple_name_type, comp_name_type}};
 
-    pub fn adj_die(data: Arc<UnitArchipelago>) -> Result<String, Error> {
+    pub fn empire_name_die(data: Arc<UnitArchipelago>) -> Result<String, Error> {
         if prelude::random_from_range(0, 1)? == 0 {
-            Ok(simple_name_type(data)?.add(" "))
+            Ok(
+                format!("{} {}",
+                simple_name_type(data.clone())?,
+                simple_name_type(data)?
+                )
+            )
         } else {
-            Ok(String::default())
+            Ok(comp_name_type(data)?)
         }
     }
 
