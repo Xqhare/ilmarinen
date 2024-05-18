8c342d3a8b3d45989766ee9c61df599f0f1b2f71
Xqhare <xqhare@web.de>
2024-05-18 21:22:07 +0200

    moved common code out

--- src/minting/presses/language_press.rs
+++ src/minting/presses/language_press.rs
@@ -2,24 +2,18 @@ use std::{sync::Arc, io::Error};
 
 use crate::unit_pools::UnitArchipelago;
 
-use self::language::language_name_die;
+use super::common::language_name_die;
 
 pub fn language_press(data: Arc<UnitArchipelago>) -> Result<String, Error> {
     language_name_die(data)
 }
 
-mod language {
+/* mod language {
     use std::{sync::Arc, io::Error};
     
     use tyche::prelude;
 
     use crate::{unit_pools::UnitArchipelago, minting::presses::common::{comp_name_type, simple_name_type}};
 
-    pub fn language_name_die(data: Arc<UnitArchipelago>) -> Result<String, Error> {
-        if prelude::random_from_range(0, 1)? == 0 {
-            simple_name_type(data)
-        } else {
-            comp_name_type(data)
-        }
-    }
-}
+    
+} */
