38afb27ff69badcafb34d14368287a059678f966
Xqhare <xqhare@web.de>
2024-05-27 20:25:02 +0200

    stabilised test suite

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,32 +5,32 @@ use ilmarinen::{WordSmith, MintingType};
 #[test]
 fn word_smith_main_test() {
     let mut word_smith = WordSmith::new(Path::new("data/")).unwrap();
-    let people = word_smith.mint(MintingType::People, 100000);
+    let people = word_smith.mint(MintingType::People, 10000);
     assert!(people.is_ok());
 
-    let place = word_smith.mint(MintingType::Place, 100000);
+    let place = word_smith.mint(MintingType::Place, 10000);
     assert!(place.is_ok());
-    let metal_alloy = word_smith.mint(MintingType::MetalAndAlloy, 100000);
+    let metal_alloy = word_smith.mint(MintingType::MetalAndAlloy, 10000);
     assert!(metal_alloy.is_ok());
-    let language = word_smith.mint(MintingType::Language, 100000);
+    let language = word_smith.mint(MintingType::Language, 10000);
     assert!(language.is_ok());
-    let artifact = word_smith.mint(MintingType::Artifact, 100000);
+    let artifact = word_smith.mint(MintingType::Artifact, 10000);
     assert!(artifact.is_ok());
-    let operation = word_smith.mint(MintingType::Operation, 100000);
+    let operation = word_smith.mint(MintingType::Operation, 10000);
     assert!(operation.is_ok());
-    let government = word_smith.mint(MintingType::Government, 100000);
+    let government = word_smith.mint(MintingType::Government, 10000);
     assert!(government.is_ok());
-    let empire = word_smith.mint(MintingType::Empire, 100000);
+    let empire = word_smith.mint(MintingType::Empire, 10000);
     assert!(empire.is_ok());
-    let ship_name = word_smith.mint(MintingType::ShipName, 100000);
+    let ship_name = word_smith.mint(MintingType::ShipName, 10000);
     assert!(ship_name.is_ok());
-    let ship_class = word_smith.mint(MintingType::ShipClass, 100000);
+    let ship_class = word_smith.mint(MintingType::ShipClass, 10000);
     assert!(ship_class.is_ok());
-    let currency = word_smith.mint(MintingType::Currency, 100000);
+    let currency = word_smith.mint(MintingType::Currency, 10000);
     assert!(currency.is_ok());
-    for entry in currency.unwrap().result {
+    /* for entry in currency.unwrap().result {
         println!("{}", entry);
         println!("");
-    };
+    }; */
 }
 
