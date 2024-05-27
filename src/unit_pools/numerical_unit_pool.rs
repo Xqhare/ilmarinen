cd5330b8b8e4306f8c2bc149c60d47665235c68a
Xqhare <xqhare@web.de>
2024-05-27 13:25:23 +0200

    renamed data field to unit_pool for consitency with lexical_unit_pool

--- src/unit_pools/numerical_unit_pool.rs
+++ src/unit_pools/numerical_unit_pool.rs
@@ -4,22 +4,22 @@ use json::JsonValue;
 #[derive(Debug, Clone)]
 pub struct NumericalUnitPool {
     pub name: String,
-    pub data: Vec<f32>,
+    pub unit_pool: Vec<f32>,
 }
 
 impl Default for NumericalUnitPool {
     fn default() -> Self {
-        NumericalUnitPool { name: "Default_Numerical_unit_Pool".to_string(), data: vec![0.0, 1.0, 1.5, -2.0] }
+        NumericalUnitPool { name: "Default_Numerical_unit_Pool".to_string(), unit_pool: vec![0.0, 1.0, 1.5, -2.0] }
     }
 }
 
 impl From<(&str, &JsonValue)> for NumericalUnitPool {
     fn from(value: (&str, &JsonValue)) -> Self {
         let name = value.0.to_string();
-        let mut data: Vec<f32> = Default::default();
+        let mut unit_pool: Vec<f32> = Default::default();
         for entry in value.1.members() {
-            data.push(entry.as_f32().expect("Wrong json entry! Has to be f32!"));
+            unit_pool.push(entry.as_f32().expect("Wrong json entry! Has to be f32!"));
         }
-        NumericalUnitPool { name, data }
+        NumericalUnitPool { name, unit_pool }
     }
 }
